7013af07e893fc9ff44b83f54e14c41e68cc2046
nishang786 <nishang786@gmail.com>
2021-11-10 00:05:24 +0530

    First commit

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -4,17 +4,24 @@ import { Capacitor } from '@capacitor/core';
 import { Directory, Filesystem } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
 import { Platform } from '@ionic/angular';
-
-
+import {HttpClient} from '@angular/common/http';
+import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
+import { finalize, map } from 'rxjs/operators';
+import { ActionSheetController, ToastController, LoadingController } from '@ionic/angular';
+import { AlertController} from '@ionic/angular';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PhotoService {
+  travUrl='https://www.travserver.com/';
   public photos: UserPhoto[] = [];
   private PHOTO_STORAGE: string = 'photos';
-
-  constructor(private platform: Platform) {}
+  file: any;
+  
+  constructor(//private photoViewer: PhotoViewer,
+    private platform: Platform ,public http : HttpClient, private loadingController: LoadingController, private toastController: ToastController) {}
 
   public async loadSaved() {
     // Retrieve cached photo array data
@@ -30,11 +37,12 @@ export class PhotoService {
           path: photo.filepath,
           directory: Directory.Data,
         });
-
+      
         // Web platform only: Load the photo as base64 data
         photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
       }
     }
+    //this.photoViewer.show('https://mysite.com/path/to/image.jpg');
   }
 
   /* Use the device camera to take a photo:
@@ -51,7 +59,8 @@ export class PhotoService {
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri, // file-based data; provides best performance
       source: CameraSource.Camera, // automatically take a new photo with the camera
-      quality: 100, // highest quality (0 to 100)
+      quality: 50, // highest quality (0 to 100)
+      saveToGallery : true
     });
 
     const savedImageFile = await this.savePicture(capturedPhoto);
@@ -64,21 +73,24 @@ export class PhotoService {
       key: this.PHOTO_STORAGE,
       value: JSON.stringify(this.photos),
     });
+    
+    console.log(this.PHOTO_STORAGE)
   }
 
   // Save picture to file on device
   private async savePicture(cameraPhoto: Photo) {
     // Convert photo to base64 format, required by Filesystem API to save
     const base64Data = await this.readAsBase64(cameraPhoto);
-
-    // Write the file to the data directory
+    
+    
     const fileName = new Date().getTime() + '.jpeg';
+    console.log(fileName)
     const savedFile = await Filesystem.writeFile({
       path: fileName,
       data: base64Data,
       directory: Directory.Data,
     });
-
+  
     if (this.platform.is('hybrid')) {
       // Display the new image by rewriting the 'file://' path to HTTP
       // Details: https://ionicframework.com/docs/building/webview#file-protocol
@@ -110,10 +122,15 @@ export class PhotoService {
       // Fetch the photo, read as a blob, then convert to base64 format
       const response = await fetch(cameraPhoto.webPath!);
       const blob = await response.blob();
-
+      const formData = new FormData();
+    const img = this.convertBlobToBase64(blob) as unknown as string
+    formData.append('file', img);
+    this.uploadImageData(formData);
       return (await this.convertBlobToBase64(blob)) as string;
     }
   }
+  
+
 
   // Delete picture by removing it from reference data and the filesystem
   public async deletePicture(photo: UserPhoto, position: number) {
@@ -127,24 +144,91 @@ export class PhotoService {
     });
 
     // delete photo file from filesystem
-    const filename = photo.filepath.substr(photo.filepath.lastIndexOf('/') + 1);
+    const filename = photo.filepath.substr(0,photo.filepath.lastIndexOf('/') + 1);
+    console.log(filename)
     await Filesystem.deleteFile({
       path: filename,
       directory: Directory.Data,
     });
+    
   }
-
+  startUpload(photo: UserPhoto) {
+    this.file.resolveLocalFilesystemUrl(photo.filepath)
+        .then(entry => {
+            ( < FileEntry > entry).file(file => this.readFile(file))
+        })
+        
+}
+  
+  readFile() {
+    // const reader = new FileReader();
+    // reader.onload = () => {
+    //     const formData = new FormData();
+    //     const imgBlob = new Blob([reader.result], {
+    //         type: file.type
+    //     });
+    //     formData.append('file', imgBlob, file.name);
+    //     this.uploadImageData(formData);
+    // };
+    // reader.readAsArrayBuffer(file);
+}
   convertBlobToBase64 = (blob: Blob) =>
     new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.onerror = reject;
       reader.onload = () => {
         resolve(reader.result);
+        
       };
+    
       reader.readAsDataURL(blob);
-    });
-}
+      
 
+    });
+
+    async presentToast() {
+      const toast = await this.toastController.create({
+        message: 'File upload complete.',
+        duration: 2000
+      })};
+      async presentToast1() {
+        const toast = await this.toastController.create({
+          message: 'File upload complete.',
+          duration: 2000
+        })};
+    
+    async uploadImageData(CData: FormData) {
+      const loading = await this.loadingController.create({
+          message: 'Uploading image...',
+      });
+      await loading.present();
+   
+      this.http.post("http://localhost:3000/uploads", { data: CData })
+          .pipe(
+              finalize(() => {
+                  loading.dismiss();
+              })
+          )
+          .subscribe(res => {
+              if (res['success']) {
+                  this.presentToast
+              }
+              else {
+                this.presentToast1
+              }
+          });
+  }
+  getDayWiseItinerary(CData: any): Observable<any> {
+    return this.http.post(this.travUrl + '/lead/insertDocumentData/', { data: CData })
+      // eslint-disable-next-line arrow-body-style
+      .pipe(map((res: any) => {
+        return res;
+      })
+      );
+  }
+  
+
+}
 export interface UserPhoto {
   filepath: string;
   webviewPath: string;
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,39 +1,38 @@
 import { Component } from '@angular/core';
 import { ActionSheetController } from '@ionic/angular';
 import { UserPhoto, PhotoService } from '../services/photo.service';
+import {OnInit} from '@angular/core'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-
-  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) {}
+export class Tab2Page implements OnInit {
+  private ffile: File;
+  constructor(public photoService: PhotoService,private http: HttpClient, public actionSheetController: ActionSheetController) {}
 
   async ngOnInit() {
     await this.photoService.loadSaved();
+    let tempPkgId = {
+      PkgId: 1
+    }
+    this.photoService.getDayWiseItinerary(tempPkgId).subscribe(respDayItineraryData => {
+      console.log("respDayItineraryData", respDayItineraryData)
+  })
   }
-
-  public async showActionSheet(photo: UserPhoto, position: number) {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Photos',
-      buttons: [{
-        text: 'Delete',
-        role: 'destructive',
-        icon: 'trash',
-        handler: () => {
-          this.photoService.deletePicture(photo, position);
-        }
-      }, {
-        text: 'Cancel',
-        icon: 'close',
-        role: 'cancel',
-        handler: () => {
-          // Nothing to do, action sheet is automatically closed
-         }
-      }]
+  onFileChange(fileChangeEvent) {
+    this.ffile = fileChangeEvent.target.files[0];
+  }
+   
+  async submitForm() {
+    
+    let formData = new FormData();
+    formData.append("photo", this.ffile, this.ffile.name);
+    this.http.post("http://localhost:3000/uploads", formData).subscribe((response) => {
+      console.log(response);
     });
-    await actionSheet.present();
   }
+ 
 }
